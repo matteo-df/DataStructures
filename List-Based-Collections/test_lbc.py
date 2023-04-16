49e6a6203d0caf789f661c78fa76ca47f55c09f0
matteo-df <matteodifilippo22@gmail.com>
2023-04-16 18:52:05 +0200

    Add Queue class + renaming

--- List-Based-Collections/test_lbc.py
+++ List-Based-Collections/test_lbc.py
@@ -1,9 +1,11 @@
 import unittest
 from linked_list import LinkedList
+from stack import Stack
+from queue import Queue
 
 
-class LinkedListTestCase(unittest.TestCase):
-    def test_str(self):
+class ListBasedCollTestCase(unittest.TestCase):
+    def test_ll_str(self):
         ll1 = LinkedList()
         ll2 = LinkedList(5)
         ll3 = LinkedList.from_list([1, 3, 5])
@@ -11,21 +13,21 @@ class LinkedListTestCase(unittest.TestCase):
         self.assertEqual(str(ll2), "5 -> None")
         self.assertEqual(str(ll3), "1 -> 3 -> 5 -> None")
 
-    def test_append(self):
+    def test_ll_append(self):
         ll = LinkedList()
         ll.append(10)
         ll.append(20)
         ll.append(30)
         self.assertEqual(str(ll), "10 -> 20 -> 30 -> None")
 
-    def test_append_left(self):
+    def test_ll_append_left(self):
         ll = LinkedList()
         ll.append_left(30)
         ll.append_left(20)
         ll.append_left(10)
         self.assertEqual(str(ll), "10 -> 20 -> 30 -> None")
 
-    def test_pop(self):
+    def test_ll_pop(self):
         ll = LinkedList.from_list([10, 20, 30])
         popped_value = ll.pop()
         self.assertEqual(popped_value, 30)
@@ -34,7 +36,7 @@ class LinkedListTestCase(unittest.TestCase):
         popped_value = ll.pop()
         self.assertEqual(popped_value, None)
 
-    def test_pop_left(self):
+    def test_ll_pop_left(self):
         ll = LinkedList.from_list([10, 20, 30])
         popped_value = ll.pop_left()
         self.assertEqual(popped_value, 10)
@@ -43,7 +45,7 @@ class LinkedListTestCase(unittest.TestCase):
         popped_value = ll.pop_left()
         self.assertEqual(popped_value, None)
 
-    def test_get_element_at(self):
+    def test_ll_get_element_at(self):
         ll = LinkedList.from_list([10, 20, 30])
         self.assertEqual(ll.get_element_at(0), 10)
         self.assertEqual(ll.get_element_at(1), 20)
@@ -54,7 +56,7 @@ class LinkedListTestCase(unittest.TestCase):
         ll = LinkedList()
         self.assertEqual(ll.get_element_at(0), None)
 
-    def test_insert_element_at(self):
+    def test_ll_insert_element_at(self):
         ll = LinkedList.from_list([10, 20, 30])
         ll.insert_element_at(0, 0)
         self.assertEqual(str(ll), "0 -> 10 -> 20 -> 30 -> None")
@@ -67,6 +69,40 @@ class LinkedListTestCase(unittest.TestCase):
         ll = LinkedList.from_list([10, 20, 30])
         ll.insert_element_at(300, 3)
         self.assertEqual(str(ll), "10 -> 20 -> 30 -> 300 -> None")
+
+    def test_stacks(self):
+        # Set up a Stack
+        stack = Stack(1)
+
+        # Test stack functionality
+        stack.push(2)
+        stack.push(3)
+        self.assertEqual(stack.pop(), 3)
+        self.assertEqual(stack.pop(), 2)
+        self.assertEqual(stack.pop(), 1)
+        self.assertEqual(stack.pop(), None)
+        stack.push(4)
+        self.assertEqual(stack.pop(), 4)
+
+    def test_queues(self):
+        # Set up a queue
+        q = Queue(1)
+        q.enqueue(2)
+        q.enqueue(3)
+
+        # Test peek
+        self.assertEqual(q.peek(), 1)
+
+        # Test dequeue
+        self.assertEqual(q.dequeue(), 1)
+
+        # Test enqueue
+        q.enqueue(4)
+        self.assertEqual(q.dequeue(), 2)
+        self.assertEqual(q.dequeue(), 3)
+        self.assertEqual(q.dequeue(), 4)
+        q.enqueue(5)
+        self.assertEqual(q.peek(), 5)
 
 
 if __name__ == '__main__':
